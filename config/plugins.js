afb0f0493cf85b53b0647f238ac18641ff076230
LeoHeffel <leo_heffel@outlook.com>
2023-04-26 07:50:22 -0300

    fix config bucket

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,15 @@
 module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      config: {
-        provider: 'aws-s3',
-        providerOptions: {
-          accessKeyId: env('SCALEWAY_ACCESS_KEY_ID'),
-          secretAccessKey: env('SCALEWAY_ACCESS_SECRET'),
-          endpoint: env('SCALEWAY_ENDPOINT'), // e.g. "s3.fr-par.scw.cloud"
-          params: {
-            Bucket: env('SCALEWAY_BUCKET'),
-          },
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
         },
       },
     },
-    // ...
-  });
+  },
+});
